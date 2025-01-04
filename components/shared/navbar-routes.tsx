c581de91f0533c91a409a15f1b3f079bb109e70a
Om <dev@grippi.io>
2025-01-04 23:50:27 +0530

    move pages to have a group

--- components/shared/navbar-routes.tsx
+++ components/shared/navbar-routes.tsx
@@ -1,10 +1,29 @@
 "use client";
 
 import { UserButton } from "@clerk/nextjs";
+import { usePathname, useRouter } from "next/navigation";
+import { Button } from "../ui/button";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
 
 export const NavbarRoutes = () => {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const isTeacherPage = pathname?.startsWith("/teacher");
+  const isPlayerPage = pathname?.includes("/chapter");
+
   return (
     <div className="flex gap-x-2 ml-auto">
+      {isTeacherPage || isPlayerPage ? (
+        <Button onClick={() => router.back()} size="sm" variant="ghost">
+          <LogOut className="w-4 h-4 mr-2" /> Exit
+        </Button>
+      ) : (
+        <Button size="sm" variant="ghost" asChild>
+          <Link href="/teacher/courses">Teacher Mode</Link>
+        </Button>
+      )}
       <UserButton />
     </div>
   );
