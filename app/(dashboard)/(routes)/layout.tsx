6772d9dd223f36e8fe1faf8b022be78a9521029f
Om <dev@grippi.io>
2025-01-05 00:10:36 +0530

    add create new course page

--- app/(dashboard)/(routes)/layout.tsx
+++ app/(dashboard)/(routes)/layout.tsx
@@ -8,14 +8,14 @@ export default function DashboardLayout({
 }) {
   return (
     <div className="h-full">
-      <div className="h-[80px] md:pl-56 fixed inset-y-0 z-50 w-full">
+      <div className="h-20 md:pl-56 fixed inset-y-0 z-50 w-full">
         <Navbar />
       </div>
 
       <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
         <Sidebar />
       </div>
-      <main className="md:pl-56 h-full">{children}</main>
+      <main className="md:pl-56 pt-20 h-full">{children}</main>
     </div>
   );
 }
