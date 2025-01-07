01b0ce2fc2210e2616bb68ea79a5f35e85764ae3
Om <dev@grippi.io>
2025-01-07 23:45:44 +0530

    add file uplaod ui component

---
+++ components/shared/file-uploader.tsx
@@ -0,0 +1,25 @@
+"use cleint";
+
+import { toast } from "sonner";
+
+import { UploadDropzone } from "@/lib/uploadthing";
+import { ourFileRouter } from "@/app/api/uploadthing/core";
+
+interface FileUploaderProps {
+  endpoint: keyof typeof ourFileRouter;
+  // onClientUploadComplete: (fileUrl: string) => void;
+  onChange: (fileUrl?: string) => void;
+}
+
+export function FileUploader({ endpoint, onChange }: FileUploaderProps) {
+  <UploadDropzone
+    endpoint={endpoint}
+    onClientUploadComplete={(res) => {
+      onChange(res?.[0].url);
+    }}
+    onUploadError={(error) => {
+      if (typeof error === "string") toast.error(error);
+      else toast.error(`${error?.message}`);
+    }}
+  />;
+}
