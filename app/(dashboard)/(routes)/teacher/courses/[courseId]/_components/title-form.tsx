5e4012215673d7fa85347e5670d9ab9dcf04a9f9
Om <dev@grippi.io>
2025-01-07 23:16:30 +0530

    add title form

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
@@ -0,0 +1,120 @@
+"use client";
+
+import { z } from "zod";
+// import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Pencil } from "lucide-react";
+import { useState } from "react";
+import { toast } from "sonner";
+import { updateCourse } from "@/actions/courses";
+
+interface Props {
+  initialData: {
+    title: string;
+  };
+  courseId: string;
+}
+
+const formSchema = z.object({
+  title: z.string().min(1, {
+    message: "Title is required",
+  }),
+});
+
+export const TitleForm = ({ courseId, initialData }: Props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  });
+
+  const { isSubmitting, isValid } = form.formState;
+
+  const toggleEditing = () => {
+    setIsEditing((prev) => !prev);
+  };
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    // console.log("🚀 ~ onSubmit ~ values:", values);
+    try {
+      // await axios.patch(`/api/courses/${courseId}`, values);
+      const course = await updateCourse(courseId, values);
+      // console.log("🚀 ~ onSubmit ~ course:", course);
+      if ((course as { error: string })?.error)
+        toast.error((course as { error: string }).error);
+      else {
+        toast.success("Course updated");
+        toggleEditing();
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
+  return (
+    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+      <div className="font-medium flex items-center justify-between">
+        Course Title
+        <Button variant={`ghost`} onClick={toggleEditing}>
+          {isEditing ? (
+            <>Cancel</>
+          ) : (
+            <>
+              <Pencil className="h-4 w-4" />
+              Edit Title
+            </>
+          )}
+        </Button>
+      </div>
+      {isEditing ? (
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-4 mt-4"
+          >
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  {/* <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+                    Course title
+                  </FormLabel> */}
+                  <FormControl>
+                    <Input
+                      disabled={isSubmitting}
+                      placeholder="e.g. 'Advanced web development'"
+                      {...field}
+                      // className="bg-zinc-300/50 border-zinc-300 focus-visible:ring-0 focus-visible:ring-offset-0"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center gap-x-2">
+              <Button disabled={!isValid || isSubmitting} type="submit">
+                Save
+              </Button>
+            </div>
+          </form>
+        </Form>
+      ) : (
+        <p className="text-sm mt-2">{initialData.title}</p>
+      )}
+    </div>
+  );
+};
