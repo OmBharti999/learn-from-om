9acd90fde33c651b17ab082ae349861af454975c
Om <dev@grippi.io>
2025-01-09 00:00:09 +0530

    Fix error value undefined

--- components/ui/combobox.tsx
+++ components/ui/combobox.tsx
@@ -21,7 +21,7 @@ import {
 
 interface Props {
   options: { label: string; value: string }[];
-  value: string;
+  value?: string;
   onChange: (value: string) => void;
 }
 
@@ -35,7 +35,7 @@ export function Combobox({ onChange, options, value }: Props) {
           variant="outline"
           role="combobox"
           aria-expanded={open}
-          className="w-[200px] justify-between"
+          className="w-full justify-between"
         >
           {value
             ? options.find((option) => option.value === value)?.label
@@ -43,7 +43,7 @@ export function Combobox({ onChange, options, value }: Props) {
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0">
+      <PopoverContent className="w-full p-0">
         <Command>
           <CommandInput placeholder="Search options..." />
           <CommandList>
