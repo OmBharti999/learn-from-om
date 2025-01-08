c53a6a452f3720ec0a5360c07bf569ae02c1b265
Om <dev@grippi.io>
2025-01-09 01:43:54 +0530

    add delete button

---
+++ components/shared/delete-button.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { X } from "lucide-react";
+import { Button } from "../ui/button";
+
+interface Props {
+  onClick: () => void;
+}
+
+export const DeleteButton = ({onClick}: Props) => {
+  return (
+    <Button
+      variant="ghost"
+      className="ml-auto hover:opacity-75 transition"
+      size="sm"
+      onClick={onClick}
+    >
+      <X className="h-4 w-4" />
+    </Button>
+  );
+};
