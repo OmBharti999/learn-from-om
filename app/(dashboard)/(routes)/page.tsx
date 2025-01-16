fae2666d38de7f188c94a41c325480dd23432059
Om <dev@grippi.io>
2025-01-16 22:56:17 +0530

    remove import

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,7 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-   <div className="">HI from dashboard </div>
-  );
+  return <div className="">HI from dashboard </div>;
 }
