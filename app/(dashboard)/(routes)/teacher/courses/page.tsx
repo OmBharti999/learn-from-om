d0db0b9bae88e3fe098b049325a1cd99f29f6d9a
Om <dev@grippi.io>
2025-01-22 00:13:12 +0530

    add table

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,13 +1,35 @@
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import React from "react";
 
-const CoursesPage = () => {
+import { Button } from "@/components/ui/button";
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
+
+const CoursesPage = async () => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("/");
+  }
+
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
   return (
     <div className="p-6">
-      <Button asChild>
+      {/* <Button asChild>
         <Link href={`/teacher/create`}>New Course</Link>
-      </Button>
+      </Button> */}
+      <DataTable columns={columns} data={courses} />
     </div>
   );
 };
