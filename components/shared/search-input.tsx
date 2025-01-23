9a52990506a9e5dd5538ea28d46293336970ddb3
Om <dev@grippi.io>
2025-01-23 23:26:28 +0530

    add search input feature

--- components/shared/search-input.tsx
+++ components/shared/search-input.tsx
@@ -1,10 +1,13 @@
 "use client";
 
 import { Search } from "lucide-react";
-import { useDebouncedCallback, useDebounce } from "use-debounce";
-import { Input } from "../ui/input";
+import queryString from "query-string";
+import { useDebounce } from "use-debounce";
 import { useEffect, useState } from "react";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+import { Input } from "../ui/input";
+import { Categories } from "@/app/(dashboard)/(routes)/search/_components";
 
 export const SearchInput = () => {
   const [value, setValue] = useState("");
@@ -12,17 +15,36 @@ export const SearchInput = () => {
   const router = useRouter();
   const pathname = usePathname();
 
-  const debouncedValue = useDebounce(value, 650);
+  const [debouncedValue, _] = useDebounce(value, 570);
 
   const currenctCategoryId = searchParams.get("categoryId");
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const url = queryString.stringifyUrl(
+      {
+        url: pathname,
+        query: {
+          categoryId: currenctCategoryId,
+          title: debouncedValue,
+        },
+      },
+      {
+        skipEmptyString: true,
+        skipNull: true,
+      }
+    );
+
+    router.push(url);
+  }, [debouncedValue, currenctCategoryId, router, pathname]);
+
   return (
     <div className="relative">
       <Search className="h-4 w-4 absolute top-3 left-3 text-slate-600" />
       <Input
         className="w-full md:w-72 pl-9 rounded-full bg-slate-100 focus-visible:ring-slate-200"
         placeholder="Search for a course..."
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
       />
     </div>
   );
