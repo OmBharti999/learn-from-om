cd09b6d32d97ee3b67055a1f7c20ad67d00aba5a
Om <ombharti888@gmail.com>
2025-01-31 23:47:50 +0530

    add mux player

--- app/(courses)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
+++ app/(courses)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
@@ -1,5 +1,10 @@
 "use client";
 
+import { cn } from "@/lib/utils";
+import MuxPlayer from "@mux/mux-player-react";
+import { Loader2, Lock } from "lucide-react";
+import { useState } from "react";
+
 interface VideoPlayerProps {
   courseId: string;
   chapterId: string;
@@ -19,5 +24,34 @@ export const VideoPlayer = ({
   title,
   nextChapter,
 }: VideoPlayerProps) => {
-  return <div>VideoPlayer</div>;
+  const [isReady, setIsReady] = useState(false);
+  return (
+    <div className="aspect-video relative">
+      {!isReady && !isLocked && (
+        <div className="absolute inset-0 bg-slate-800 flex items-center justify-center z-20">
+          <Loader2 className="w-8 h-8 text-secondary animate-spin" />
+        </div>
+      )}
+
+      {isLocked && (
+        <div className="absolute inset-0 bg-slate-800 flex items-center justify-center flex-col gap-y-2 text-secondary">
+          <Lock className="w-8 h-8" />
+          <p className="text-sm">This chapter is locked.</p>
+        </div>
+      )}
+
+      {!isLocked && (
+        <MuxPlayer
+          title={title}
+          playbackId={playbackId}
+          className={cn({
+            hidden: !isReady,
+          })}
+          onCanPlay={() => setIsReady(true)}
+          onEnded={() => {}}
+          autoPlay
+        />
+      )}
+    </div>
+  );
 };
