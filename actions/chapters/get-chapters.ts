ce4cc2bcf685fc0431f3be99fa07951dfc0f7b15
Om <ombharti888@gmail.com>
2025-01-31 23:36:48 +0530

    add get chapter component

---
+++ actions/chapters/get-chapters.ts
@@ -0,0 +1,115 @@
+import type { Attachment, Chapter } from "@prisma/client";
+
+import { db } from "@/lib/db";
+
+interface GetChapters {
+  courseId: string;
+  userId: string;
+  chapterId: string;
+}
+
+export const getChapters = async ({
+  chapterId,
+  courseId,
+  userId,
+}: GetChapters) => {
+  try {
+    const purchase = await db.purchase.findUnique({
+      where: {
+        userId_courseId: {
+          userId,
+          courseId,
+        },
+      },
+    });
+    const course = await db.course.findUnique({
+      where: {
+        id: courseId,
+        isPublished: true,
+      },
+      select: {
+        price: true,
+      },
+    });
+    const chapter = await db.chapter.findUnique({
+      where: {
+        id: chapterId,
+        isPublished: true,
+      },
+      //   include: {
+      //     muxData: {
+      //       select: {
+      //         assetId: true,
+      //       },
+      //     },
+      //   },
+    });
+
+    if (!chapter || !course) {
+      throw new Error("Chapter or course not found");
+    }
+
+    let muxData = null;
+    let attachments: Attachment[] = [];
+    let nextChapter: Chapter | null = null;
+
+    if (purchase) {
+      attachments = await db.attachment.findMany({
+        where: {
+          courseId,
+        },
+      });
+    }
+
+    if (chapter.isFree || purchase) {
+      muxData = await db.muxData.findUnique({
+        where: {
+          chapterId,
+        },
+      });
+
+      nextChapter = await db.chapter.findFirst({
+        where: {
+          courseId,
+          isPublished: true,
+          position: {
+            gt: chapter?.position,
+          },
+        },
+        orderBy: {
+          position: "asc",
+        },
+      });
+    }
+
+    const userProgress = await db.userProgress.findUnique({
+      where: {
+        userId_chapterId: {
+          userId,
+          chapterId,
+        },
+      },
+    });
+
+    return {
+      chapter,
+      course,
+      muxData,
+      attachments,
+      nextChapter,
+      userProgress,
+      purchase,
+    };
+  } catch (error) {
+    console.log(`🚀 ~ getChapters ~ error:`, error);
+    return {
+      chapter: null,
+      course: null,
+      muxData: null,
+      attachments: [],
+      nextChapter: null,
+      userProgress: null,
+      purchase: null,
+    };
+  }
+};
