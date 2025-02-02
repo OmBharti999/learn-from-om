6ee0e92dc04bb8b812db823578d43d6363d8f152
Om <ombharti888@gmail.com>
2025-02-02 12:47:48 +0530

    add progress component

--- app/(courses)/courses/[courseId]/_components/course-sidebar.tsx
+++ app/(courses)/courses/[courseId]/_components/course-sidebar.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 import type { Chapter, Course, UserProgress } from "@prisma/client";
 
 import { CourseSidebarItem } from "./course-sidebar-item";
+import { CourseProgress } from "@/components/shared/course-progress";
 
 import { db } from "@/lib/db";
 
@@ -29,6 +30,11 @@ export const CourseSidebar = async ({ course, progressCount }: Props) => {
     <div className="h-full border-r flex flex-col overflow-y-auto shadow-sm">
       <div className="p-8 flex flex-col border-b">
         <h1 className="font-semibold">{course.title}</h1>
+        {purchase && (
+          <div className="mt-10">
+            <CourseProgress value={progressCount} varient="success" />
+          </div>
+        )}
       </div>
       <div className="flex flex-col w-full">
         {course.chapters.map((chapter) => (
