f41db706487356cb6a4fb7b7e24cc68c0929a6c1
Om <ombharti888@gmail.com>
2025-02-02 16:49:36 +0530

    protecting our frontend so users cant access teacher page

---
+++ app/(dashboard)/(routes)/teacher/layout.tsx
@@ -0,0 +1,13 @@
+import { isTeacher } from "@/lib/teacher";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+export default async function TeacherLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { sessionClaims } = await auth();
+  if (isTeacher(sessionClaims?.sub)) return redirect("/");
+  return <>{children}</>;
+}
